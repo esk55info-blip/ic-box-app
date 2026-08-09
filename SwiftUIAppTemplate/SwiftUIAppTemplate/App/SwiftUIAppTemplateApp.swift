743568b534b9c4cf1234433a3b1c753a36cfd524
esk55info-blip <esk55info@gmail.com>
2026-08-09 15:52:25 +0300

    Update SwiftUIAppTemplateApp.swift

--- SwiftUIAppTemplate/SwiftUIAppTemplate/App/SwiftUIAppTemplateApp.swift
+++ SwiftUIAppTemplate/SwiftUIAppTemplate/App/SwiftUIAppTemplateApp.swift
@@ -1,20 +1,3 @@
-//
-//  SwiftUIAppTemplateApp.swift
-//  SwiftUIAppTemplate
-//
-//  Created by Yuri Cernov on 09/03/2026.
-//
-
 import SwiftUI
 
-@main
-struct SwiftUIAppTemplateApp: App {
-
-    private let container = AppContainer()
-
-    var body: some Scene {
-        WindowGroup {
-            AppRootView(container: container)
-        }
-    }
-}
+// Neutralized template entry point
